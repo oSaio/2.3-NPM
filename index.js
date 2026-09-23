8f457030bdc65086674996985c7335317bf2216f
Saif <137000350+oSaio@users.noreply.github.com>
2026-09-23 21:49:56 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// const { from } = require('node:stream/iter');
+// var generateName = require('sillyname');
+
+// import generateName from "sillyname";
+// console.log(`My name is ${sillyName}.`);
+// var sillyName = generateName();
+import { randomSuperhero } from 'superheroes';
+
+console.log(`I am ${randomSuperhero()}`)
